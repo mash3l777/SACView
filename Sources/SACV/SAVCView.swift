2b9032794075baac2a8a3c4ab3c7f79e6ed90d86
Mashal <mash3l777@gmail.com>
2021-03-14 09:54:59 +0400

    .

--- Sources/SACV/SAVCView.swift
+++ Sources/SACV/SAVCView.swift
@@ -11,7 +11,7 @@ public protocol SACVDalegate {
      func btnAddAction(_ sender: Any)
 }
 
-public struct PredefindSentence {
+open struct PredefindSentence {
      let shortCut: String
      let sentence: String
 }
