03ff4fa31c5f873e8a110d3a3cdfd3d23d5b7b68
Mashal <mash3l777@gmail.com>
2021-03-14 10:31:59 +0400

    .

--- Sources/SACV/PredefindSentence.swift
+++ Sources/SACV/PredefindSentence.swift
@@ -8,11 +8,7 @@
 import Foundation
 
 public struct PredefindSentence {
-    public let shortCut: String
-    public let sentence: String
-    
-    init(shortCut:String,sentence:String) {
-        self.shortCut = ""
-        self.sentence = ""
-    }
+    public var shortCut: String = ""
+    public var sentence: String = ""
+
 }
