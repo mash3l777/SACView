faf489049ce0280050ceecd89ca6f62f1a28a2ce
Mashal <mash3l777@gmail.com>
2021-03-15 09:17:26 +0400

    add Resources file

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SACV",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "SACVTests",
             dependencies: ["SACV"]),
